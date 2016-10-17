1ef44e16ae68346bbf0c8e27c5d2cb25f62e2651
Kevin <Kevin@10.0.0.201>
2016-10-17 02:13:42 -0400

    commit

--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -18,36 +18,34 @@ public class Student_Test {
 
 	@BeforeClass
 	public static void setup() throws PersonException {
-		
+
 		Course class1 = new Course();
 		Course class2 = new Course();
 		Course class3 = new Course();
 		ArrayList classList = new ArrayList();
-		
-		
+
 		classList.add(class1);
 		classList.add(class2);
 		classList.add(class3);
-		
+
 		UUID id1 = UUID.randomUUID();
 		Date semdate1 = new Date();
 		Date semdate2 = new Date();
-		
+
 		Semester fall = new Semester(id1, semdate1, semdate2);
 		Semester spring = new Semester(id1, semdate1, semdate2);
 		ArrayList semList = new ArrayList();
-		
+
 		semList.add(fall);
 		semList.add(spring);
-		
-		
+
 		Section classa = new Section(id1, id1, id1, 5);
 		Section classb = new Section(id1, id1, id1, 5);
 		Section classc = new Section(id1, id1, id1, 5);
 		Section classd = new Section(id1, id1, id1, 5);
 		Section classe = new Section(id1, id1, id1, 5);
 		Section classf = new Section(id1, id1, id1, 5);
-		
+
 		ArrayList<Section> secList = new ArrayList<Section>();
 		secList.add(classa);
 		secList.add(classc);
@@ -55,21 +53,29 @@ public class Student_Test {
 		secList.add(classb);
 		secList.add(classd);
 		secList.add(classf);
-		
-		
-		Student student_one = new Student("Matt", "John", "Damon", semdate2, eMajor.BUSINESS, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_two = new Student("Steve", "Frankie", "Waler", semdate2, eMajor.CHEM, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_three = new Student("Kylo", "Luke", "Bob", semdate2, eMajor.BUSINESS, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_four = new Student("Frank", "Hardy", "Nugan", semdate2, eMajor.CHEM, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_five = new Student("John", "Hav", "Williams", semdate2, eMajor.BUSINESS, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_six = new Student("Franklin", "Mack", "Neilson", semdate2, eMajor.COMPSI, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_seven = new Student("James", "Joe", "Frost", semdate2, eMajor.BUSINESS, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_eight = new Student("John", "Apple", "Martin", semdate2, eMajor.NURSING,"101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_nine = new Student("Fender", "Gibson", "Les", semdate2, eMajor.CHEM, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
-		Student student_ten = new Student("Will", "Depp", "Sparrow", semdate2, eMajor.PHYSICS, "101 fake lane", "(555)-555-5555", "fakeemail@gmail.com");
+
+		Student student_one = new Student("Matt", "John", "Damon", semdate2, eMajor.BUSINESS, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_two = new Student("Steve", "Frankie", "Waler", semdate2, eMajor.CHEM, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_three = new Student("Kylo", "Luke", "Bob", semdate2, eMajor.BUSINESS, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_four = new Student("Frank", "Hardy", "Nugan", semdate2, eMajor.CHEM, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_five = new Student("John", "Hav", "Williams", semdate2, eMajor.BUSINESS, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_six = new Student("Franklin", "Mack", "Neilson", semdate2, eMajor.COMPSI, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_seven = new Student("James", "Joe", "Frost", semdate2, eMajor.BUSINESS, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_eight = new Student("John", "Apple", "Martin", semdate2, eMajor.NURSING, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_nine = new Student("Fender", "Gibson", "Les", semdate2, eMajor.CHEM, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
+		Student student_ten = new Student("Will", "Depp", "Sparrow", semdate2, eMajor.PHYSICS, "101 fake lane",
+				"(555)-555-5555", "fakeemail@gmail.com");
 		ArrayList<Student> student_List = new ArrayList<Student>();
-		
-		
+
 		student_List.add(student_one);
 		student_List.add(student_two);
 		student_List.add(student_three);
